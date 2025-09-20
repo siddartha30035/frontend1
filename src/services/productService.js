762af53e628cf8d128982d8d2bc6fe1c0d989730
ANKATHA SIDDARTHA <2300030035@kluniversity.in>
2025-09-20 09:49:48 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-54-92-221-206.compute-1.amazonaws.com:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://54.164.159.155:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
